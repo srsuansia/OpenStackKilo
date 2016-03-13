914d79a250aeb5d5e8716aa55e36129444d73760
soumya suansia <soumya.suansia@gmail.com>
2016-03-13 14:09:37 +0530

    Open stack kilo install script

---
+++ openstack-centos7-kilo.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+## Pre-requsite: Install Centos 7 minimal
+## NOTE:
+## Check the following before proceeding.
+## 1> whether "ifconfig" command worning or not ?
+## If not:
+##      run the following command:
+##      $   yum provides ifconfig
+##      or
+##      $   yum whatprovides ifconfig
+##
+##      $   yum install net-tools
+##
+## Testing:
+##      $ ifonfig -a
+
+echo "Stop NetworkManager ..."
+systemctl stop NetworkManager
+systemctl disable NetworkManager
+systemctl status NetworkManager
+
+
+echo "Configure yum repository .."
+yum install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+
+echo "Configure OpenStack repository .."
+yum install http://rdo.fedorapeople.org/openstack-kilo/rdo-release-kilo.rpm
+
+echo "Configure NTP .."
+yum -y install ntp
+ntpdate -u 0.centos.pool.ntp.org
+systemctl enable ntpd
+systemctl start ntpd
+systemctl status ntpd
+
+echo "Install openvswitch .."
+yum -y install openvswitch
+
+echo "Configure openswitch .."
+systemctl enable openvswitch.service
+
+
+        
